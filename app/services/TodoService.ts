438d7e915d8515f129689e50df7915c24c20cddc
220003 <220003pb@yse-c.net>
2024-02-07 12:03:31 +0900

    test

---
+++ app/services/TodoService.ts
@@ -0,0 +1,39 @@
+import { Todo } from "../models/Todo";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
+
+export const getTodos = async () => {
+    const url = `${API_URL}/api/todo/get`; // APIのURLを設定
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            return await response.json();
+        } else {
+            throw new Error('Failed to fetch todos');
+        }
+    } catch (error) {
+        console.error(error);
+        return []; // エラーが発生した場合は空の配列を返す
+    }
+}
+
+export const postTodos = async (todos: Todo[]) => {
+    if (!todos || todos.length === 0) return;
+    const url = `${API_URL}/api/todo/add`; // APIのURLを設定
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(todos),
+        });
+        if (response.ok) {
+            return await response.json();
+        } else {
+            throw new Error('Failed to save todos');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
